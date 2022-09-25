96f22a405b60c9b186b49c557d99d6863dec8927
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2022-09-25 15:19:43 -0700

    Day 20 Project 2 overview - Gradients.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,18 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0 ) {
-                Color.red
-                Color.blue
-            }
-            Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+        // Types of Gradients
+        
+        // 1. Linear 1
+//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+        
+        // 2. Linear 2
+//        LinearGradient(gradient: Gradient(stops: [
+//            .init(color: .white, location: 0.45),
+//            .init(color: .black, location: 0.55)
+//        ]), startPoint: .top, endPoint: .bottom)
+        
+        // 3. Radial
+        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+        
+        // 4. Angular
+//        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+        
     }
+    // **** All gradients can use stops ****
 }
 
 struct ContentView_Previews: PreviewProvider {
